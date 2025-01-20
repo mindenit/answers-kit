5546577c286d20fe8c1b4597c92d690523cfcc04
perkinson1251 <tttrashutin@gmail.com>
2025-01-20 16:46:00 +0200

    feat: add SubjectsModule with CRUD operations for subjects

--- packages/answers-kit/src/index.ts
+++ packages/answers-kit/src/index.ts
@@ -2,6 +2,8 @@ import { CoursesModule } from './modules/courses/index.js';
 import type { ICoursesModule } from './modules/courses/types.js';
 import { FacultiesModule } from './modules/faculties/index.js';
 import type { IFacultiesModule } from './modules/faculties/types.js';
+import { SubjectsModule } from './modules/subjects/index.js';
+import type { ISubjectModule } from './modules/subjects/types.js';
 
 interface AnswersKitConfig {
   apiUrl: string;
@@ -11,6 +13,7 @@ export class AnswersKit {
   private readonly apiUrl: string;
   readonly courses: ICoursesModule;
   readonly faculties: IFacultiesModule;
+  readonly subjects: ISubjectModule;
 
   constructor(config?: AnswersKitConfig) {
     if (config && config.apiUrl.endsWith('/')) {
@@ -23,5 +26,6 @@ export class AnswersKit {
 
     this.courses = new CoursesModule(this.apiUrl);
     this.faculties = new FacultiesModule(this.apiUrl);
+    this.subjects = new SubjectsModule(this.apiUrl);
   }
 }
---
+++ packages/answers-kit/src/modules/subjects/types.ts
@@ -0,0 +1,47 @@
+import type { RequestHeaders, SortingOptions, Subject } from '@/types.js';
+
+interface FIndManySubjectArgs {
+  sorting?: SortingOptions;
+}
+
+interface CreateSubjectValues {
+  name: string;
+  brief: string;
+  facultyId: number;
+}
+
+interface CreateSubjectArgs {
+  data: CreateSubjectValues;
+  headers: RequestHeaders;
+}
+
+interface UpdateSubjectValues extends CreateSubjectValues {
+  id: number;
+}
+
+interface UpdateSubjectArgs {
+  data: UpdateSubjectValues;
+  headers: RequestHeaders;
+}
+
+interface DeleteSubjectArgs {
+  id: number;
+  headers: RequestHeaders;
+}
+
+interface ISubjectModule {
+  findMany: (args?: FIndManySubjectArgs) => Promise<Subject[]>;
+  createOne: (args: CreateSubjectArgs) => Promise<Subject>;
+  updateOne: (args: UpdateSubjectArgs) => Promise<Subject>;
+  deleteOne: (args: DeleteSubjectArgs) => Promise<Subject>;
+}
+
+export type {
+  CreateSubjectArgs,
+  CreateSubjectValues,
+  DeleteSubjectArgs,
+  FIndManySubjectArgs,
+  ISubjectModule,
+  UpdateSubjectArgs,
+  UpdateSubjectValues,
+};
